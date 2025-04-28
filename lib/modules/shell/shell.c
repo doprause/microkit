108dcc75e511c050fbdc25e57d22a9886a95a146
Dominik Prause <dominik.prause@viavisolutions.com>
2025-04-28 13:27:14 +0200

    Adds shell module

---
+++ lib/modules/shell/shell.c
@@ -0,0 +1,300 @@
+#include "shell.h"
+
+#include "microkit/lib/assert.h"
+#include "microkit/lib/utils/termcolor.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+extern ShellCommand COMMANDS[];
+extern Size COMMAND_COUNT;
+
+extern ShellLogger LOGGERS[];
+extern Size LOGGER_COUNT;
+
+static const int STATUS_ERROR_UNKNOWN_COMMAND = -1000;
+
+static const char* COMMAND_PROMPT = "$ ";                                               // colorize("host-emulator > ", TERMCOLOR_GREY)
+static const char* COMMAND_FAILED = colorize("\nCommand failed\n", TERMCOLOR_RED);      // "\x1b[91mCommand failed\x1b[0m\r\n";
+static const char* COMMAND_UNKNOWN = colorize("\nCommand unknown\n", TERMCOLOR_YELLOW); // "\x1b[36mCommand unknown\x1b[0m\r\n";
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+struct ShellObject {
+   ShellCommand* commands;
+   Size commandCount;
+   bool commandPending;
+   char* commandPrompt;
+   ShellLogger* loggers;
+   bool loggerActive;
+   Size loggerCount;
+   bool loggerPending;
+   int loggerTimerId;
+   char commandBuffer[CLI_MAX_BUFFER_SIZE];
+   char receiveBuffer[CLI_MAX_BUFFER_SIZE];
+   char* receiveBufferPointer;
+   // PowerSimulatorInstance powerSimulator;
+   UartDevice serial;
+   // SupervisorInstance supervisor;
+   // SoftwareTimerInstance softwareTimer;
+   const char* prompt;
+   char terminator;
+};
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+struct ShellObject SHELL_OBJECT;
+const ShellModule SHELL = &SHELL_OBJECT;
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+static Status private_interpret(const ShellModule instance) {
+
+   UInt8 argc = 0;
+   char* argv[CLI_ARGC_MAX_COUNT];
+
+   // Get the first token (cmd name)
+   argv[argc] = strtok(instance->commandBuffer, " ");
+
+   // Return immediately if the first token is NULL
+   if (argv[argc] == NULL) {
+      return STATUS_OK;
+   }
+
+   // Walk through the other tokens (parameters)
+   while ((argv[argc] != NULL) && (argc < CLI_ARGC_MAX_COUNT)) {
+      argv[++argc] = strtok(NULL, " ");
+   }
+
+   // Search the command table for a matching command
+   for (Size i = 0; i < instance->commandCount; i++) {
+      if (strcmp(argv[0], instance->commands[i].command) == 0) {
+         // Found a match, execute the associated function.
+         return instance->commands[i].handler(argc, argv, instance);
+      }
+   }
+
+   return STATUS_ERROR_UNKNOWN_COMMAND;
+}
+
+#include "microkit/lib/contracts/platform/stdio.h"
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+static void private_print(const ShellModule shell, const char* message) {
+
+   // Uart.send(shell->serial, (UInt8*)message, strlen(message)); // TODO: This doesn't work for some reason
+
+   while (*message != '\0') {
+      if (*message == '\r') {
+         mkit_platform_stdio_put('\n');
+      } else {
+         mkit_platform_stdio_put(*message);
+      }
+      message++;
+   }
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+// static void onLoggerTimerElapsed(const SoftwareTimerInstance timer, void* target, uint32 time) {
+
+//    UNUSED_ARG(timer);
+//    UNUSED_ARG(time);
+
+//    ShellModule instance = (ShellModule)target;
+
+//    ASSERT_NOT_NULL_POINTER(instance);
+
+//    instance->loggerPending = true;
+// }
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+static void init(
+    const ShellModule instance,
+    //  const SoftwareTimerInstance softwareTimer,
+    const UartDevice serial,
+    const char* prompt,
+    const char terminator
+    //  const SupervisorInstance supervisor,
+    //  const PowerSimulatorInstance powerSimulator
+) {
+
+   ASSERT_NOT_NULL_POINTER(instance);
+
+   instance->commands = COMMANDS;
+   instance->commandCount = COMMAND_COUNT;
+   instance->commandPending = false;
+   instance->loggers = LOGGERS;
+   instance->loggerActive = false;
+   instance->loggerCount = LOGGER_COUNT;
+   instance->loggerPending = false;
+   instance->loggerTimerId = 0;
+   instance->receiveBufferPointer = instance->receiveBuffer;
+   instance->serial = serial;
+   // instance->softwareTimer = softwareTimer;
+   // instance->supervisor = supervisor;
+   // instance->powerSimulator = powerSimulator;
+   // volatile int test = strcmp(prompt, "123");
+   instance->prompt = strcmp(prompt, "") != 0 ? prompt : COMMAND_PROMPT,
+   instance->terminator = terminator;
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+static void stop(const ShellModule instance) {
+
+   ASSERT_NOT_NULL_POINTER(instance);
+
+   // SoftwareTimer.stop(instance->softwareTimer, instance->loggerTimerId);
+   Uart.stop(instance->serial);
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+static void start(const ShellModule instance) {
+
+   ASSERT_NOT_NULL_POINTER(instance);
+
+   UartConfig config = {
+       .baudrate = MKIT_UART_BAUDRATE_115200,
+       .databits = MKIT_UART_DATABITS_8,
+       .stopbits = MKIT_UART_STOPBITS_1,
+       .parity = MKIT_UART_PARITY_NONE,
+   };
+
+   Uart.start(instance->serial, config);
+
+   // instance->loggerTimerId = SoftwareTimer.start(
+   //     instance->softwareTimer,
+   //     onLoggerTimerElapsed,
+   //     instance,
+   //     1000,
+   //     TIMER_PERIODIC);
+
+   private_print(instance, instance->prompt);
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+static void process_character(const ShellModule instance, Int32 character) {
+
+   if (character < 0) {
+      return;
+   }
+
+   // Disable all loggers if logging is active
+   if (instance->loggerActive) {
+      for (Size i = 0; i < instance->loggerCount; i++) {
+         instance->loggers[i].disable(instance);
+      }
+
+      instance->loggerActive = false;
+      instance->loggerPending = false;
+
+      // Flush input buffer
+      UInt8 character;
+      while (0 != Uart.receive(instance->serial, &character, 1)) {
+         // Do nothing
+      };
+   }
+
+   if (character == instance->terminator) {
+      // Message terminator.
+      if (!instance->commandPending) {
+         *(instance->receiveBufferPointer) = '\0';                 // Terminate the message
+         strcpy(instance->commandBuffer, instance->receiveBuffer); // Copy message to command buffer
+
+         instance->receiveBufferPointer = instance->receiveBuffer; // Reset receive buffer pointer to beginning
+         instance->commandPending = true;
+      }
+   } else if (character == '\b') {
+      // Backspace. Delete character.
+      if (instance->receiveBufferPointer > instance->receiveBuffer) {
+         instance->receiveBufferPointer--;
+      }
+   } else {
+      // Normal character received, add to buffer
+      if ((instance->receiveBufferPointer - instance->receiveBuffer) < CLI_MAX_BUFFER_SIZE) {
+         *(instance->receiveBufferPointer) = character;
+         instance->receiveBufferPointer++;
+      }
+      // Echo the received character to console
+      const char echo[] = {character, '\0'};
+      private_print(instance, echo);
+   }
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+static void process(const ShellModule instance) {
+
+   ASSERT_NOT_NULL_POINTER(instance);
+
+   UInt8 character;
+   Status uartRxStatus = Uart.receive(instance->serial, &character, 1);
+
+   while (uartRxStatus == STATUS_OK && !instance->commandPending) {
+      process_character(instance, character);
+      uartRxStatus = Uart.receive(instance->serial, &character, 1);
+   }
+
+   // Process the next command, if any
+   // commandPending is set when a terminator was detected
+   if (instance->commandPending) {
+      Status status = private_interpret(instance);
+
+      if (status == STATUS_ERROR) {
+         private_print(instance, COMMAND_FAILED);
+      } else if (status == STATUS_ERROR_UNKNOWN_COMMAND) {
+         private_print(instance, COMMAND_UNKNOWN);
+      }
+
+      private_print(instance, instance->prompt);
+
+      instance->commandPending = false;
+   }
+
+   // Process loggers
+   // for (Size i = 0; i < instance->loggerCount; i++) {
+   //    if (instance->loggers[i].status != NULL && instance->loggers[i].status(instance)) {
+   //       instance->loggerActive = true;
+
+   //       if (instance->loggerPending) {
+   //          instance->loggers[i].handler(instance);
+   //       }
+   //    }
+   // }
+
+   // Reset the loggers pending flag
+   // if (instance->loggerPending) {
+   //    instance->loggerPending = false;
+   // }
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+void respond(const ShellModule instance, const char* message) {
+
+   ASSERT_NOT_NULL_POINTER(instance);
+
+   private_print(instance, message);
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+// PowerSimulatorInstance powerSimulator(const ShellModule instance) {
+
+//    ASSERT_NOT_NULL_POINTER(instance);
+
+//    return instance->powerSimulator;
+// }
+
+// /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+// SupervisorInstance supervisor(const ShellModule instance) {
+
+//    ASSERT_NOT_NULL_POINTER(instance);
+
+//    return instance->supervisor;
+// }
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+const ShellInterface Shell = {
+    .init = init,
+    //  .disable = disable,
+    .start = start,
+    .stop = stop,
+    .process = process,
+    .respond = respond,
+    //  .powerSimulator = powerSimulator,
+    //  .supervisor = supervisor,
+};
---
+++ lib/modules/shell/shell.h
@@ -0,0 +1,162 @@
+
+
+#ifndef MKIT_SHELL_H
+#define MKIT_SHELL_H
+
+// #include "framework/software_timer.h"
+
+#include "microkit/lib/contracts/platform/drivers/uart.h"
+// #include "platform/api/serial.h"
+
+#ifndef CLI_MAX_BUFFER_SIZE
+#define CLI_MAX_BUFFER_SIZE 128
+#endif
+
+#ifndef CLI_ARGC_MAX_COUNT
+#define CLI_ARGC_MAX_COUNT 8
+#endif
+
+#ifndef CLI_MAX_RESPONSE_LENGTH
+#define CLI_MAX_RESPONSE_LENGTH 256
+#endif
+
+typedef struct ShellObject* ShellModule;
+
+/**
+ * @brief Macro to define a command.
+ * @param commandName The command name (string).
+ * @param handlerFunction The command handler (function pointer).
+ * @param descriptionText The command description (string).
+ */
+#define COMMAND(commandName, handlerFunction, descriptionText) {.command = commandName, .handler = handlerFunction, .description = descriptionText}
+
+/**
+ * @brief Command handler function declaration.
+ * @param argc The number of arguments.
+ * @param argv The arguments.
+ * @param cli A reference to the shell module instance.
+ */
+typedef Status (*CommandHandlerFunction)(int argc, char** argv, const ShellModule shell);
+
+/**
+ * @brief Command structure, consisting of a command name and a function
+ * pointer to a command handler function.
+ */
+typedef struct {
+   const char* command;
+   CommandHandlerFunction handler;
+   char* description;
+} ShellCommand;
+
+/**
+ * @brief Macro to define a logger.
+ * @param handlerFunction The logger handler (function pointer).
+ */
+#define LOGGER(loggerName, handlerFunction, disableFunction, statusFunction) {.logger = loggerName, .handler = handlerFunction, .disable = disableFunction, .status = statusFunction}
+
+/**
+ * @brief Logger disable function declaration.
+ * @param shell A reference to the shell module instance.
+ */
+typedef Status (*LoggerDisableFunction)(const ShellModule shell);
+
+/**
+ * @brief Logger handler function declaration.
+ * @param shell A reference to the shell module instance.
+ */
+typedef Status (*LoggerHandlerFunction)(const ShellModule shell);
+
+/**
+ * @brief Logger status function declaration.
+ * @param shell A reference to the shell module instance.
+ */
+typedef bool (*LoggerStatusFunction)(const ShellModule shell);
+
+/**
+ * @brief Logger structure, consisting of a logger handler function.
+ * @param loggerName The logger name (string).
+ * @param handlerFunction The logger handler (function pointer).
+ */
+typedef struct {
+   char* logger;
+   LoggerDisableFunction disable;
+   LoggerHandlerFunction handler;
+   LoggerStatusFunction status;
+} ShellLogger;
+
+/**
+ * @brief Shell interface.
+ */
+typedef struct {
+   /**
+    * @brief Initializes the given shell module instance.
+    * @param softwareTimer The software timer to be used.
+    * @param serial The serial communication interface to be used with the shell.
+    * @param terminator The command terminator string.
+    */
+   void (*init)(
+       const ShellModule shell,
+       const UartDevice serial,
+       const char* prompt,
+       const char terminator);
+   // ShellModule (*init)(
+   //     const SoftwareTimerInstance softwareTimer,
+   //     const SerialInstance serial,
+   //     const char terminator,
+   //     const SupervisorInstance supervisor,
+   //     const PowerSimulatorInstance powerSimulator);
+
+   // /**
+   //  * @brief Destroys the instance and frees the memory.
+   //  * @param instance The instance to destroy.
+   //  */
+   // void (*destroy)(const ShellModule shell);
+
+   /**
+    * @brief Starts the shell.
+    * @param instance The instance.
+    */
+   void (*start)(const ShellModule shell);
+
+   /**
+    * @brief Stops the shell.
+    * @param instance The instance.
+    */
+   void (*stop)(const ShellModule shell);
+
+   /**
+    * @brief Process commands if any.
+    * This function must be called regulary in a superloop or a operating system task.
+    * @param instance The instance to process.
+    */
+   void (*process)(const ShellModule shell);
+
+   /**
+    * @brief Sends a response to the CLI.
+    * @param instance The instance.
+    * @param message The response message.
+    */
+   void (*respond)(const ShellModule shell, const char* message);
+
+   // /**
+   //  * @brief Gets the power simulator instance.
+   //  * Typically used to get the power simulator instance within a command handler.
+   //  * @param instance The instance.
+   //  * @returns The power simulator instance.
+   //  */
+   // PowerSimulatorInstance (*powerSimulator)(const ShellModule shell);
+
+   // /**
+   //  * @brief Gets the supervisor instance.
+   //  * Typically used to get the supervisor instance within a command handler.
+   //  * @param instance The instance.
+   //  * @returns The supervisor instance.
+   //  */
+   // SupervisorInstance (*supervisor)(const ShellModule shell);
+
+} ShellInterface;
+
+extern const ShellInterface Shell;
+
+// ----------------------------------------------------------------------------
+#endif
